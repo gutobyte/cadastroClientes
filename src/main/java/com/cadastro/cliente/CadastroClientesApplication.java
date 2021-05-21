d62a2320ac5d767e0ba340a215428c59d094d7cf
Gustavo Moreira <gustavo.ms@protonmail.com>
2021-05-20 22:15:42 -0300

    Add/repo spring data-jpa,security e web

--- src/main/java/com/cadastro/cliente/CadastroClientesApplication.java
+++ src/main/java/com/cadastro/cliente/CadastroClientesApplication.java
@@ -2,9 +2,19 @@ package com.cadastro.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CadastroClientesApplication {
+	
+	
+	@GetMapping(value="/")
+	public String HelloWorld() {
+		
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroClientesApplication.class, args);
